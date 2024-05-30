26bd4a62b529f039dfa444c0b90b9283de8c60a6
Thorne <ExistentialEnso@users.noreply.github.com>
2024-05-30 00:27:53 -0400

    Home page refinements.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
 import Link from 'next/link'
+import { RiTwitterXFill } from "react-icons/ri"
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center justify-between p-24">
             <h1 className="text-6xl font-bold mb-2">achieve3</h1>
             
-            <p className="text-lg my-4">
-                achiev3 is a standardized protocol for defining and rewarding achievements onchain. It is open source (MIT License), decentralized, and composable, enabling anyone to tap into the robust set of contracts onchain to define 
-                their perfect rewards for users accomplishing tasks. Founded by <Link href="https://ens0.me" className="text-red-800 hover:text-red-600" target="_blank">ens0.eth</Link>.
+            <p className="text-lg my-4 mx-8">
+                achiev3 is a protocol for defining and rewarding achievements on-chain. It is open source (MIT License), decentralized, and composable, enabling anyone to tap into the robust set of contracts onchain to define 
+                their perfect rewards for users accomplishing tasks.
             </p>
 
-            <h2 className="text-4xl font-bold my-4">Repositories</h2>
+            <h2 className="text-3xl font-bold my-4">repos</h2>
+
+            <p className="my-2">All code is MIT licensed unless otherwise noted.</p>
+
             <ul className="text-lg list-disc">
                 <li className="my-2">
-                    <Link href="https://github.com/achiev3protocol/achiev3-foundry" className="text-red-800 hover:text-red-600" target="_blank">foundry</Link>
+                    <Link href="https://github.com/achiev3protocol/achiev3-foundry" className="text-red-800 hover:text-red-600" target="_blank">foundry</Link> - protocol smart contracts
                 </li>
             </ul>
+
+            <h2 className="text-3xl font-bold my-4">socials</h2>
+
+            <a href="https://x.com/achiev3protocol/" target="_blank" title="Twitter"><RiTwitterXFill style={{width: "32px", height: "32px"}} /></a>
         </main>
     )
 }
