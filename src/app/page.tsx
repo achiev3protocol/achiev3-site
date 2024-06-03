ba317647a6be5596c6b0619e8e61cbdff4a39bba
Thorne <ExistentialEnso@users.noreply.github.com>
2024-06-03 10:37:04 -0400

    Add FAQ section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,18 @@ export default function Home() {
                 Coming Summer 2024 to Base. Founded by <Link className="text-red-800 hover:text-red-600" href="https://ens0.me">ens0.eth</Link>.
             </p>
 
+            <h2 className="text-3xl font-bold my-4">faqs</h2>
+
+            <dl>
+                <dt>How does it work?</dt>
+                <dd>Creators register a new achievement set, which creates a new contract that they own! From there, the owner or other addresses with delegated unlocking privileges can unlock achievements within the set. These can be unlocked through off-chain conditions by calling the unlock function directly &mdash; but the delegated addresses can be smart contracts handling events on-chain!</dd>
+            </dl>
+
+            <dl>
+                <dt>How much does it cost?</dt>
+                <dd>Unlocking achievements only costs gas &mdash; and will remain that way forever. A small fee will be charged to register a new achievement set with the official implementation of the protocol. In the future, we may offer things like support packages or APIs for a modest price.</dd>
+            </dl>
+
             <h2 className="text-3xl font-bold my-4">repos</h2>
 
             <p className="my-2">All code is MIT licensed unless otherwise noted.</p>
